e6f49aae569029adffa8b2d9ebe5e815a2030f2c
Evgeniy Feder <evgeniyfeder@localhost.localdomain>
2019-10-19 16:22:38 +0300

    Add implicit euler method

--- task1/src/methods/euler.py
+++ task1/src/methods/euler.py
@@ -1,19 +1,33 @@
 from methods.common import SystemConfig, Point
 
 
-def one_step(p: Point, config: SystemConfig):
-    return Point(x=p.x + config.dt * config.dx_dt(p),
-                 y=p.y + config.dt * config.dy_dt(p),
-                 z=p.z + config.dt * config.dz_dt(p),
-                 t=p.t + config.dt)
+def _one_step(p_n: Point, dp_n: Point, config: SystemConfig):
+    return Point(x=p_n.x + config.dt * config.dx_dt(dp_n),
+                 y=p_n.y + config.dt * config.dy_dt(dp_n),
+                 z=p_n.z + config.dt * config.dz_dt(dp_n),
+                 t=p_n.t + config.dt)
 
 
-def solve_euler(x0, y0, z0, t0, config=SystemConfig()):
+def solve_euler_explicit(x0, y0, z0, t0, config=SystemConfig()):
     p = Point(x0, y0, z0, t0)
 
     pts = [p]
     while p.t < config.max_t:
-        p = one_step(p, config)
+        p = _one_step(p, p, config)
         pts.append(p)
 
+    return pts
+
+
+def solve_euler_implicit(x0, y0, z0, t0, config=SystemConfig()):
+    p = Point(x0, y0, z0, t0)
+
+    pts = [p]
+    while p.t < config.max_t:
+        p_next = _one_step(p, p, config)
+
+        p = _one_step(p, p_next, config)
+        p.t = p_next.t
+
+        pts.append(p)
     return pts
