45b4d99adc0798543292213d578069c246c39811
Evgeniy Feder <dzaba67@yandex.ru>
2019-12-17 21:32:40 +0300

    Add solve interface

---
+++ cw/src/methods/solve.py
@@ -0,0 +1,38 @@
+import itertools
+from typing import Callable, Iterator
+
+import numpy as np
+import pandas as pd
+from eq_system import SystemConfig
+
+
+def _solve(begin_ts: np.array,
+           begin_xs: np.array,
+           method: Callable[[np.array, np.array, SystemConfig], np.array],
+           config: SystemConfig) -> Iterator[pd.DataFrame]:
+    cur_ts = begin_ts
+    cur_xs = begin_xs
+    start_frame = pd.DataFrame(
+        {"time": 0, "x": config.dz * i, "T": begin_ts[i], "X": begin_xs[i]}
+        for i in range(config.num_points))
+    yield start_frame
+
+    for i in range(1, config.num_iter + 1):
+        t = config.dt * i
+        cur_ts, cur_xs = method(cur_ts, cur_xs, config)
+
+        new_frame = pd.DataFrame(
+            {"time": t, "x": config.dz * j, "T": cur_ts[j], "X": cur_xs[j]}
+            for j in range(config.num_points))
+        yield new_frame
+
+
+def solve(begin_ts: np.array,
+          begin_xs: np.array,
+          method: Callable[[np.array, np.array, SystemConfig], np.array],
+          config: SystemConfig,
+          freq: int = None) -> pd.DataFrame:
+    frames = _solve(begin_ts, begin_xs, method, config)
+    if freq is not None:
+        frames = itertools.islice(frames, None, None, freq)
+    return pd.concat(list(frames))
