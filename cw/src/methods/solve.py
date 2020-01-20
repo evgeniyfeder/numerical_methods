c8683cdee224c5151d862bafe30cdfe2eceda4b6
Golovin Pavel <gopavel0@gmail.com>
2020-01-20 12:20:54 +0300

    a little bit improve performance

--- cw/src/methods/solve.py
+++ cw/src/methods/solve.py
@@ -10,7 +10,8 @@ from tqdm import tqdm_notebook
 def _solve(begin_ts: np.array,
            begin_xs: np.array,
            method: Callable[[np.array, np.array, SystemConfig], np.array],
-           config: SystemConfig) -> Iterator[pd.DataFrame]:
+           config: SystemConfig,
+           freq: int) -> Iterator[pd.DataFrame]:
     cur_ts = begin_ts
     cur_xs = begin_xs
 
@@ -22,11 +23,11 @@ def _solve(begin_ts: np.array,
     for i in tqdm_notebook(range(1, config.num_iter + 1)):
         t = config.dt * i
         cur_xs, cur_ts = method(cur_ts, cur_xs, config)
-
-        new_frame = pd.DataFrame(
-            {"time": t, "x": config.dz * j, "T": cur_ts[j], "X": cur_xs[j]}
-            for j in range(config.num_points))
-        yield new_frame
+        if i % freq == 0:
+            new_frame = pd.DataFrame(
+                {"time": t, "x": config.dz * j, "T": cur_ts[j], "X": cur_xs[j]}
+                for j in range(config.num_points))
+            yield new_frame
 
 
 def solve(*, begin_ts: np.array,
@@ -34,7 +35,5 @@ def solve(*, begin_ts: np.array,
           method: Callable[[np.array, np.array, SystemConfig], np.array],
           config: SystemConfig,
           freq: int = None) -> pd.DataFrame:
-    frames = _solve(begin_ts, begin_xs, method, config)
-    if freq is not None:
-        frames = itertools.islice(frames, None, None, freq)
+    frames = _solve(begin_ts, begin_xs, method, config, freq=freq)
     return pd.concat(list(frames))
